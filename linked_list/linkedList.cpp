57a28448f2b590f14eec151e6eeea90a1b08fba9
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-02-22 20:44:02 -0800

    create project for linkedList

--- linked_list/linkedList.cpp
+++ linked_list/linkedList.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+// linkedList.cpp
+// CIT 66
+// Spring 25
+// by Mark Edmunds
+//
 
+#include <iostream>
+#include <array>
+
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // TODO create a program that does the following:
+    /*
+    Singly Linked List Define a Node struct with an integer data field and a pointer to the next node.
+    Implement functions to: Insert a node at the end of the list.
+    Delete a node by value. Display all nodes in the list.
+     */
     return 0;
 }
