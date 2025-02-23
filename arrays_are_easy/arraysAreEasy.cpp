d10dfa41b15988db17d0581896bc36c18ab1629a
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-02-22 20:21:28 -0800

    create project for arraysAreEasy

--- arrays_are_easy/arraysAreEasy.cpp
+++ arrays_are_easy/arraysAreEasy.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+// arraysAreEasy.cpp
+// CIT 66
+// Spring 25
+// by Mark Edmunds
+// Program creates a 3-D int array
 
+#include <iostream>
+#include <array>
+
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // TODO: code up arrays to get to a 3-D int array.
     return 0;
 }
