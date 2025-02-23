903c1bf00f09a75e90a92ef0d439e8481ebb67c5
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-02-22 20:31:02 -0800

    create project for pointersArePowerful

--- pointers_are_powerful/pointersArePowerful.cpp
+++ pointers_are_powerful/pointersArePowerful.cpp
@@ -1,6 +1,19 @@
+// pointersArePowerful.cpp
+// CIT 66
+// Spring 25
+// by Mark Edmunds
+//
 #include <iostream>
+#include <array>
+
+using  namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // TODO create program that does the following:
+    /*
+     Write a program that declares an integer variable a and a pointer p to an integer.
+     Assign the address of a to p. Print the address stored in p and the value of a using p.
+     Change the value of a using p and print the new value of a.
+     */
     return 0;
 }
